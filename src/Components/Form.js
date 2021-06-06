94071091462052fa112a9e7f270691610ec315a5
Keshav Reddy <keshavreddy8899@gmail.com>
2021-06-06 20:01:59 +0530

    remove previous

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Form extends Component {
+    render() {
+        return (
+            <div>
+                Contact Form
+            </div>
+        )
+    }
+}
+export default Form
